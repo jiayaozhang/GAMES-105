5f4665266b9a25dffaf4d9f2f7db09481c57ef3e
jiayaozhang <jiayao.zhang@kaust.edu.sa>
2022-12-22 00:21:58 +0800

    update Lab1 Task 1 and Task 2

--- lab1/task2_inverse_kinematics.py
+++ lab1/task2_inverse_kinematics.py
@@ -175,7 +175,7 @@ def main():
     # part2
     # part2(viewer, 'data/walk60.bvh')
     
-    # bonus(viewer, np.array([0.5, 0.5, 0.5]), np.array([0, 0.5, 0.5]))
+    bonus(viewer, np.array([0.5, 0.5, 0.5]), np.array([0, 0.5, 0.5]))
 
 if __name__ == "__main__":
     main()
